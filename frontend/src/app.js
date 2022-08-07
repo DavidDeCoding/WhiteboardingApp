30988dbcc30ec750c9b27e206a040b5fe9b7154f
daviddecoding <daviddecoding@gmail.com>
2022-08-07 12:42:18 -0700

    Added sending websocket call

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import CanvasDraw from 'react-canvas-draw';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
+import { v4 as uuidv4 } from 'uuid';
 
-function onMessage() {
-  window.saveableCanvas.loadSaveData();
+const url = "wss://5d4l39qktg.execute-api.us-west-1.amazonaws.com/dev";
+const ws = new W3CWebSocket(url);
+
+function onMessage(message) {
+    const lines = JSON.parse(message.data);
+
+
 }
 
-function onChange() {
-  console.log(window.saveableCanvas.getSaveData());
+function onChange(event) {
+    console.log(event);
+    const message = window.saveableCanvas.getSaveData();
+    ws.send(JSON.stringify({
+        action: "onMessage",
+        message: JSON.stringify(message)
+    }));
 }
 
 class App extends Component {
     constructor(props) {
         super(props);
+    }
+
+    componentWillMount() {
+        ws.onopen = (event) => {
+            console.log(`Connected to ${url}}`);
+        };
+
+        ws.onmessage = (message) => {
+            onMessage(message);
+        };
     }
 
     render() {
